000f6fb16a38e54f95f79795cd8443f15df9bdc0
Shushas <Shushasrp@gmail.com>
2023-09-25 18:33:43 +0300

    Complete calculator function

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  firstNumb: 0,
+  secondNumb: 0,
+  read: function(a, b) {
+    this.firstNumb = a;
+    this.secondNumb = b;
+  },
+  sum: function() {
+    return this.firstNumb + this.secondNumb;
+  },
+  mul: function() {
+    return this.firstNumb * this.secondNumb;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
