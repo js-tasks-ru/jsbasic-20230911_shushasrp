325bc26f91b2dab8ccd340c67130d1d75222343d
Shushas <Shushasrp@gmail.com>
2023-09-25 17:07:32 +0300

    check spam func

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
-function checkSpam(str) {
-  // ваш код...
+function checkSpam(value) {
+  const spamText = ["1xBet", "XXX"];
+  const string = String(value).toLowerCase();
+
+  for (let i = 0; i < spamText.length; i++) {
+    if (string.includes(spamText[i].toLowerCase())) {
+      return true;
+      break;
+    }
+  }
+
+  return false;
 }
