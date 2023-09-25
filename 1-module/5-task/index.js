6a1b6f3875c8be2acc9119e9c048537303aeff7e
Shushas <Shushasrp@gmail.com>
2023-09-25 17:26:29 +0300

    complete truncate function

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  const suffix = '…';
+
+  if (str.length > 20) {
+    str = str.slice(0, maxlength - suffix.length) + suffix;
+  }
+
+  return str;
 }
